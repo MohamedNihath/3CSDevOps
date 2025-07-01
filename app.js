6e08507e8f40485dbe9f8c8da316ac99f1d0fa39
MohamedNihath <mohamednihath0308@gmail.com>
2025-07-01 07:02:48 +0530

    Add CI Pipline with Github Actions

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get('/',(req,res) => {
 
 })
 
-app.listen(PORT,()=>{
+const server=app.listen(PORT,()=>{
     console.log(`Server is Runnig on Port ${PORT}`)
 })
+
+module.exports={app,server}
---
+++ test/app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest')
+const {app,server}= require('../app')
+
+describe('Get /',()=>{
+    it("Should return 200 status and the correct message",async () =>{
+        const response= await request(app).get("/");
+        expect(response.status).toBe(200);
+         expect(response.text).toBe("Welcome to ShopEasy from TM World");
+
+    })
+
+})
+afterAll(()=>{
+    server.close
+})
